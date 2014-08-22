dc2468071f3992739fa49605577aca8caea93a30
khiemvu <khiemvx@qsoftvietnam.com>
2014-08-22 15:14:11 +0700

    init project

---
+++ gen/com/us/ata/Manifest.java
@@ -0,0 +1,7 @@
+/*___Generated_by_IDEA___*/
+
+package com.us.ata;
+
+/* This stub is only used by the IDE. It is NOT the Manifest class actually packed into the APK */
+public final class Manifest {
+}
